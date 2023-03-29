f63e3ad8093b4f368ecbfec2d959a6ae34f22032
Matthew Bardoe <mbardoe@gmail.com>
2023-03-29 07:29:36 -0400

    Updated output

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from armbot import ArmBot
 
 
 if __name__=="__main__":
+    # This code should run and produce the following output:
+    # x: 1 y: 2
+    # x: 4 y: 6
+    # beep
+    # x: 5 y: 8 arm: 0, claw: False
+    # x: 5 y: 8 arm: 10, claw: False
+    # x: 5 y: 8 arm: 10, claw: True
+    # x: 5 y: 8 arm: 10, claw: False
+    # beep
+
     robbie=BaseRobot(1,2)
     print(robbie)
     robbie.move(3,4)
